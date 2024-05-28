87ee70ac5c15676ff9a6a6354ae925a9efc8d151
drswan1 <s1089249@stud.sbg.ac.at>
2024-05-28 16:46:34 +0200

    URL dictionary Updates

--- src/webparts/tabs/TabsWebPart.ts
+++ src/webparts/tabs/TabsWebPart.ts
@@ -15,9 +15,9 @@ export interface ITabsWebPartProps {
 
 export default class TabsWebPart extends BaseClientSideWebPart<ITabsWebPartProps> {
   private tabsHashMap: { [key: string]: string } = {
-    orientiert: "https://labplusacat.sharepoint.com/sites/StudentPlace/Orientiert/",
-    informiert: "https://labplusacat.sharepoint.com/sites/StudentPlace/Informiert/",
-    verbunden: "https://labplusacat.sharepoint.com/sites/StudentPlace/Verbunden/",
+    orientiert: "https://plusacat.sharepoint.com/sites/StudentPlace/Orientiert/",
+    informiert: "https://plusacat.sharepoint.com/sites/StudentPlace/Informiert/",
+    verbunden: "https://plusacat.sharepoint.com/sites/StudentPlace/Verbunden/",
   };
 
   private setSelectedTab(tabKey: string): void {
