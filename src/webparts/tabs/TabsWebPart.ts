6b8ecd9e07e754fe5e672e7446137b35c1ee6a33
drswan1 <you@example.com>
2024-10-08 13:10:19 +0200

    2.0.0.0V Complete code refactor, updated styling, private method encapsulation outside render function

--- src/webparts/tabs/TabsWebPart.ts
+++ src/webparts/tabs/TabsWebPart.ts
@@ -14,149 +14,97 @@ export interface ITabsWebPartProps {
 }
 
 export default class TabsWebPart extends BaseClientSideWebPart<ITabsWebPartProps> {
-  private tabsHashMap: { [key: string]: string } = {
-    orientiert: "https://plusacat.sharepoint.com/Orientiert/SitePages/Home.aspx",
-    informiert: "https://plusacat.sharepoint.com/Informiert/SitePages/Home.aspx",
-    verbunden: "https://plusacat.sharepoint.com/Verbunden/SitePages/Home.aspx",
-  };
-
-  private setSelectedTab(tabKey: string): void {
-    // Remove 'selected' class from all tabs
-    Object.keys(this.tabsHashMap).forEach((key) => {
-      const tabElement = this.domElement.querySelector(`[data-tab="${key}"]`);
-      if (tabElement) {
-        tabElement.classList.remove(styles.selected);
-        tabElement.classList.add(styles.notSelected);
-      }
-    });
-
-    // Add 'selected' class to the specified tab
-    const selectedTabElement = this.domElement.querySelector(`[data-tab="${tabKey}"]`);
-    if (selectedTabElement) {
-      selectedTabElement.classList.remove(styles.notSelected);
-      selectedTabElement.classList.add(styles.selected);
-    }
-  }
-
-  private getCurrentTabKey(currentURL: string): string | undefined {
-    // Find the key corresponding to the current URL
-    return Object.keys(this.tabsHashMap).find((key: string) => this.tabsHashMap[key] === currentURL);
-  }
-
   public render(): void {
-
-    if (typeof window !== 'undefined') {
-      const currentURL = window.location.href;
-
-    // Find the key for the current URL
-    const currentTabKey = this.getCurrentTabKey(currentURL);
-
-    // Update the class name based on the current tab key
-    if (currentTabKey) {
-      this.setSelectedTab(currentTabKey);
-    }
-
-
 
     // Render the HTML
     this.domElement.innerHTML = `
 
     <section>
-    <div class="${styles.container}">
-
-      
-
-     
-
-      
-      <div class="${styles.marker}">
-        <div id="${styles.left}"><a href="${this.tabsHashMap.orientiert}" data-tab="orientiert" class="${currentTabKey === 'orientiert' ? styles.selected : styles.notSelected}">
-        <div class="${styles.content}">
-          <img class="${styles.icons}" src="${require('./assets/Orientiert.png')}" />
-          <div id="${styles.siteName}"> Orientiert </div>
-        </div>
-      </a></div>
-        <div id="${styles.middle}"> <a href="${this.tabsHashMap.informiert}" data-tab="informiert" class="${currentTabKey === 'informiert' ? styles.selected : styles.notSelected}">
-        <div class="${styles.content}">
-          <img class="${styles.icons}" src="${require('./assets/Informiert.svg')}" />
-          <div id="${styles.siteName}"> Informiert </div>
-        </div>
-      </a></div>
-        <div id="${styles.right}"><a href="${this.tabsHashMap.verbunden}" data-tab="verbunden" class="${currentTabKey === 'verbunden' ? styles.selected : styles.notSelected}">
-        <div class="${styles.content}">
-          <img class="${styles.icons}" src="${require('./assets/Verbunden.svg')}" /> 
-          <div id="${styles.siteName}"> Verbunden </div>
-        </div>
-      </a></div>
+         <div class="${styles.container}">
+        <input type="radio" id="${styles.orientiert}" name="option" value="orientiert">
+        <label class="${styles.box}" for="orientiert">
+        <a href="${'https://plusacat.sharepoint.com/sites/StudentPlace/Orientiert'}">
+            <img class=${styles.logo} src="${require('./assets/Orientiert.png')}" alt="Orientiert">
+            <h1>Orientiert</h1>
+        </a>
+        </label>
+        
+        <input type="radio" id="${styles.informiert}" name="option" value="informiert">
+        <label class="${styles.box}" for="informiert">
+          <a href="${'https://plusacat.sharepoint.com/sites/StudentPlace/Informiert'}">
+            <img class=${styles.logo} src="${require('./assets/Informiert.svg')}" alt="Informiert">
+            <h1>Informiert</h1>
+          </a>
+        </label>
+        <input type="radio" id="${styles.verbunden}" name="option" value="verbunden">
+        <label class="${styles.box}" for="verbunden">
+          <a href="${'https://plusacat.sharepoint.com/sites/StudentPlace/Verbunden'}">
+            <img class=${styles.logo} src="${require('./assets/Verbunden.svg')}" alt="Verbunden">
+            <h1>Verbunden</h1>
+          </a>
+        </label>
     </div>
-  </div>
   </section>`;
-  console.log(currentTabKey);
 
-  const leftMarker = document.getElementById(styles.left);
-  const middleMarker = document.getElementById(styles.middle);
-  const rightMarker = document.getElementById(styles.right);
-
-  if (currentTabKey === 'orientiert') {
-    (leftMarker as HTMLElement).style.borderTop = "3px solid #c4c4c4";
-    (leftMarker as HTMLElement).style.borderRadius = "32px";
-    (leftMarker as HTMLElement).style.width = "calc(50%)";
-    (leftMarker as HTMLElement).style.justifyContent = "center";
-    
-
-    (middleMarker as HTMLElement).style.borderBottom = "2px solid #c4c4c4";
-    (middleMarker as HTMLElement).style.borderBottomLeftRadius = "32px";
-    (middleMarker as HTMLElement).style.boxShadow = "0px 15px 36px -20px #e7e7e7";
-    (middleMarker as HTMLElement).style.width = "calc(50%)";
-    (middleMarker as HTMLElement).style.justifyContent = "center";
-
-    (rightMarker as HTMLElement).style.borderBottom = "2px solid #c4c4c4";
-    (rightMarker as HTMLElement).style.boxShadow = "0px 15px 36px -20px #e7e7e7";
-    (rightMarker as HTMLElement).style.width = "calc(50%)";
-    (rightMarker	 as HTMLElement).style.justifyContent = "center";
-    console.log('orientiert');
+this.checkRadioButton();
+// this. cssSelector();
   }
 
-  if (currentTabKey === 'informiert') {
-    (leftMarker as HTMLElement).style.borderBottom = "2px solid #c4c4c4";
-    (leftMarker as HTMLElement).style.borderBottomRightRadius = "32px";
-    (leftMarker as HTMLElement).style.boxShadow = "0px 15px 36px -20px #e7e7e7";
-    (leftMarker as HTMLElement).style.width = "calc(50%)";
-    (leftMarker as HTMLElement).style.justifyContent = "center";
+  private checkRadioButton(): void {
+    const OrientiertUrl = 'https://plusacat.sharepoint.com/sites/StudentPlace/Orientiert';
+    const InformiertUrl = 'https://plusacat.sharepoint.com/sites/StudentPlace/Informiert';
+    const VerbundenUrl = 'https://plusacat.sharepoint.com/sites/StudentPlace/Verbunden';
 
-    (middleMarker as HTMLElement).style.borderTop = "3px solid #c4c4c4";
-    (middleMarker as HTMLElement).style.borderRadius = "32px";
-    (middleMarker as HTMLElement).style.width = "calc(50%)";
-    (middleMarker as HTMLElement).style.justifyContent = "center";
+    if (window.location.href.includes(OrientiertUrl)) {
+        const orientiertRadio = document.getElementById(styles.orientiert) as HTMLInputElement;
+        if (orientiertRadio) {
+            orientiertRadio.checked = true;
+        }
+    }
 
-    (rightMarker as HTMLElement).style.borderBottom = "2px solid #c4c4c4";
-    (rightMarker as HTMLElement).style.borderBottomLeftRadius = "32px";
-    (rightMarker as HTMLElement).style.boxShadow = "0px 15px 36px -20px #e7e7e7";
-    (rightMarker as HTMLElement).style.width = "calc(50%)";
-    (rightMarker	 as HTMLElement).style.justifyContent = "center";
-    console.log('informiert');
-  }
+    if (window.location.href.includes(InformiertUrl)) {
+        const informiertRadio = document.getElementById(styles.informiert) as HTMLInputElement;
+        if (informiertRadio) {
+            informiertRadio.checked = true;
+        }
+    }
 
-  if (currentTabKey === 'verbunden') {
-    (leftMarker as HTMLElement).style.borderBottom = "2px solid #c4c4c4";
-    (leftMarker as HTMLElement).style.boxShadow = "0px 15px 36px -20px #e7e7e7";
-    (leftMarker as HTMLElement).style.width = "calc(50%)";
-    (leftMarker as HTMLElement).style.justifyContent = "center";
-
-    (middleMarker as HTMLElement).style.borderBottom = "2px solid #c4c4c4";
-    (middleMarker as HTMLElement).style.borderBottomRightRadius = "32px";
-    (middleMarker as HTMLElement).style.boxShadow = "0px 15px 36px -20px #e7e7e7";
-    (middleMarker as HTMLElement).style.width = "calc(50%)";
-    (middleMarker as HTMLElement).style.justifyContent = "center";
-
-    (rightMarker as HTMLElement).style.borderTop = "3px solid #c4c4c4";
-    (rightMarker as HTMLElement).style.borderRadius = "32px";
-    (rightMarker as HTMLElement).style.width = "calc(50%)";
-    (rightMarker	 as HTMLElement).style.justifyContent = "center";
-    console.log('verbunden');
-  }
-  }
+    if (window.location.href.includes(VerbundenUrl)) {
+        const verbundenRadio = document.getElementById(styles.verbunden) as HTMLInputElement;
+        if (verbundenRadio) {
+            verbundenRadio.checked = true;
+        }
+    }
 }
+
+// private cssSelector(): void {
+//   document.querySelectorAll('input[type="radio"]').forEach((radio) => {
+//       radio.addEventListener('change', () => {
+//           const orientiertLabel = document.querySelector(`label[for="${styles.orientiert}"]`) as HTMLElement;
+//           const informiertLabel = document.querySelector(`label[for="${styles.informiert}"]`) as HTMLElement;
+//           const verbundenLabel = document.querySelector(`label[for="${styles.verbunden}"]`) as HTMLElement;
+
+//           // Reset border radius
+//           if (orientiertLabel) orientiertLabel.style.borderBottomRightRadius = '';
+//           if (informiertLabel) {
+//               informiertLabel.style.borderBottomLeftRadius = '';
+//               informiertLabel.style.borderBottomRightRadius = '';
+//           }
+//           if (verbundenLabel) verbundenLabel.style.borderBottomLeftRadius = '';
+
+//           if ((document.getElementById(styles.informiert) as HTMLInputElement).checked) {
+//               if (orientiertLabel) orientiertLabel.style.borderBottomRightRadius = '26px';
+//               if (verbundenLabel) verbundenLabel.style.borderBottomLeftRadius = '26px';
+//           }
+//           if ((document.getElementById(styles.verbunden) as HTMLInputElement).checked) {
+//               if (informiertLabel) informiertLabel.style.borderBottomRightRadius = '26px';
+//           }
+//           if ((document.getElementById(styles.orientiert) as HTMLInputElement).checked) {
+//               if (informiertLabel) informiertLabel.style.borderBottomLeftRadius = '26px';
+//           }
+//       });
+//   });
+// }
 
  
 
